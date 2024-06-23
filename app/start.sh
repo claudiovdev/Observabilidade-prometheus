89e9f930320f45acf3f6cfc1a076c1b7d4aef138
claudiovdev <c.viniciusmagalhaes@gmail.com>
2024-06-23 17:10:57 -0300

    adicionando actuator

--- app/start.sh
+++ app/start.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-java -jar -Xms128M -Xmx128M -XX:PermSize=64m -XX:MaxPermSize=128m -Dspring.profiles.active=prod target/forum.jar
+java -jar -Xms128M -Xmx128M -XX:PermSize=64m -XX:MaxPermSize=128m -Dspring.profiles.active=prod target\forum.jar
+java -Xms128M -Xmx128M -jar forum.jar --spring.profiles.active=prod
